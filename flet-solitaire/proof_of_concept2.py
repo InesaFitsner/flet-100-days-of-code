7f1a81978886bb9f6dc41643f518bace373e0762
Inesa Fitsner <inesa@appveyor.com>
2022-12-08 20:17:08 -0800

    Update proof_of_concept2.py

--- flet-solitaire/proof_of_concept2.py
+++ flet-solitaire/proof_of_concept2.py
@@ -23,24 +23,27 @@ def main(page: ft.Page):
         """Brings draggable card to the top of the stack"""
         list.remove(item)
         list.append(item)
+        for i in list:
+            print(i)
+        page.update()
 
     def find_close_space(item, spaces):
         """Returns closest space's top and left; if no close spaces returns original top and left"""
-        coordinates = (item.data.top, item.data.left)
+        # coordinates = (item.data.top, item.data.left)
         for space in spaces:
             if abs(item.top - space.top) < 20 and abs(item.left - space.left) < 20:
                 return (space.top, space.left)
 
-        return coordinates
+        return item.data.top, item.data.left
 
     def start_drag(e: ft.DragStartEvent):
-        move_on_top(e.control, cards)
+        move_on_top(e.control, controls)
         page.update()
 
-    def check_proximity(e: ft.DragEndEvent):
-
-        e.control.top = find_close_space(e.control, spaces)[0]
-        e.control.left = find_close_space(e.control, spaces)[1]
+    def drop(e: ft.DragEndEvent):
+        coordinates = find_close_space(e.control, spaces)
+        e.control.top = coordinates[0]
+        e.control.left = coordinates[1]
         e.control.data.top = e.control.top
         e.control.data.left = e.control.left
         page.update()
@@ -50,50 +53,36 @@ def main(page: ft.Page):
         e.control.left = max(0, e.control.left + e.delta_x)
         e.control.update()
 
+    spaces = []
+
     # top spaces (final piles)
-    space1 = ft.Container(width=65, height=100, left=0, top=0, border=ft.border.all(1))
+    x = 0
+    for i in range(3):
+        spaces.append(
+            ft.Container(width=65, height=100, left=x, top=0, border=ft.border.all(1))
+        )
+        x += 100
 
-    space2 = ft.Container(
-        width=65, height=100, left=100, top=0, border=ft.border.all(1)
-    )
-
-    space3 = ft.Container(
-        width=65, height=100, left=200, top=0, border=ft.border.all(1)
-    )
     # bottom spaces (piles)
-    space4 = ft.Container(
-        width=65, height=100, left=0, top=150, border=ft.border.all(1)
-    )
+    y = 0
+    for i in range(3):
+        spaces.append(
+            ft.Container(width=65, height=100, left=y, top=150, border=ft.border.all(1))
+        )
+        y += 100
 
-    space5 = ft.Container(
-        width=65, height=100, left=100, top=150, border=ft.border.all(1)
-    )
-
-    space6 = ft.Container(
-        width=65, height=100, left=200, top=150, border=ft.border.all(1)
-    )
-
-    s1 = Space(space1)
-    s2 = Space(space2)
-    s3 = Space(space3)
-    s4 = Space(space4)
-    s5 = Space(space5)
-    s6 = Space(space6)
-    space1.data = s1
-    space2.data = s2
-    space3.data = s3
-    space4.data = s4
-    space5.data = s5
-    space6.data = s6
+    s = []
+    for i in range(len(spaces)):
+        space_object = Space(spaces[i])
+        spaces[i].data = space_object
+        s.append(space_object)
 
     card1 = ft.GestureDetector(
         mouse_cursor=ft.MouseCursor.MOVE,
         drag_interval=10,
         on_pan_update=on_pan_update2,
         on_pan_start=start_drag,
-        on_pan_end=check_proximity,
-        # left=0,
-        # top=0,
+        on_pan_end=drop,
         content=ft.Container(bgcolor=ft.colors.GREEN, width=65, height=100),
     )
 
@@ -102,26 +91,23 @@ def main(page: ft.Page):
         drag_interval=10,
         on_pan_update=on_pan_update2,
         on_pan_start=start_drag,
-        on_pan_end=check_proximity,
-        # left=100,
-        # top=100,
+        on_pan_end=drop,
         content=ft.Container(bgcolor=ft.colors.AMBER, width=65, height=100),
     )
-    c1 = Card(card1, space4)
+    c1 = Card(card1, spaces[4])
     card1.data = c1
     card1.top = c1.top
     card1.left = c1.left
 
-    c2 = Card(card2, space5)
+    c2 = Card(card2, spaces[5])
     card2.data = c2
     card2.top = c2.top
     card2.left = c2.left
 
-    spaces = [space1, space2, space3, space4, space5, space6]
     cards = [card1, card2]
+    controls = spaces + cards
 
-    # page.add(ft.Stack(cards, width=1000, height=500))
-    page.add(ft.Stack(spaces + cards, width=1000, height=500))
+    page.add(ft.Stack(controls, width=1000, height=500))
 
 
 ft.app(target=main)
