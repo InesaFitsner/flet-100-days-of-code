4e4d08f9e7e2ba5f8bb32de7bdb1b9ca5b08402d
Inesa Fitsner <inesa@appveyor.com>
2022-12-08 17:16:16 -0800

    Update main.py

--- flet-solitaire/main.py
+++ flet-solitaire/main.py
@@ -1,44 +1,58 @@
 import flet as ft
 
+
 def main(page: ft.Page):
-    def on_pan_update1(e: ft.DragUpdateEvent):
-        c.top = max(0, c.top + e.delta_y)
-        c.left = max(0, c.left + e.delta_x)
-        #print(f"ctop1 {c.top} cleft1 {c.left}")
-        # if c.top > 100:
-        #     print ("Too low")
-        c.update()
-    
-    def check_top(e: ft.DragEndEvent):
-        if c.top > c3.top:
-            print ("Too low")
-        c.update()
+    def move_on_top(item, list):
+        list.remove(item)
+        list.append(item)
+
+    def start_drag(e: ft.DragStartEvent):
+        move_on_top(e.control, cards)
+        page.update()
+
+    def check_proximity(e: ft.DragEndEvent):
+        if (
+            abs(e.control.top - space.top) < 20
+            and abs(e.control.left - space.left) < 20
+        ):
+            print("Close enough")
+        page.update()
 
     def on_pan_update2(e: ft.DragUpdateEvent):
         e.control.top = max(0, e.control.top + e.delta_y)
         e.control.left = max(0, e.control.left + e.delta_x)
         e.control.update()
 
-    gd = ft.GestureDetector(
-        mouse_cursor=ft.MouseCursor.MOVE,
-        drag_interval=50,
-        on_pan_update=on_pan_update1,
-        on_pan_end=check_top
+    space = ft.Container(
+        width=50, height=50, left=200, top=200, border=ft.border.all(5)
     )
 
-    c = ft.Container(gd, bgcolor=ft.colors.AMBER, width=50, height=50, left=0, top=0)
-
-    c3 = ft.Container(bgcolor=ft.colors.BLACK, width=50, height=50, left=200, top=200)
-
-    gd1 = ft.GestureDetector(
+    card1 = ft.GestureDetector(
         mouse_cursor=ft.MouseCursor.MOVE,
         drag_interval=10,
         on_pan_update=on_pan_update2,
-        left=100,
-        top=100,
-        content=ft.Container(bgcolor=ft.colors.BLUE, width=50, height=50),
+        on_pan_start=start_drag,
+        on_pan_end=check_proximity,
+        left=0,
+        top=0,
+        # content=ft.Container(bgcolor=ft.colors.BLUE, width=50, height=50),
+        content=ft.Container(bgcolor=ft.colors.GREEN, width=50, height=50),
     )
 
-    page.add( ft.Stack([c, gd1, c3], width=1000, height=500))
+    card2 = ft.GestureDetector(
+        mouse_cursor=ft.MouseCursor.MOVE,
+        drag_interval=10,
+        on_pan_update=on_pan_update2,
+        on_pan_start=start_drag,
+        on_pan_end=check_proximity,
+        left=100,
+        top=100,
+        content=ft.Container(bgcolor=ft.colors.AMBER, width=50, height=50),
+    )
+
+    cards = [card1, card2, space]
+
+    page.add(ft.Stack(cards, width=1000, height=500))
+
 
 ft.app(target=main)
