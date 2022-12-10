0238a6091b4eb82c0d10ef37b0fdcad67bed8de1
Inesa Fitsner <inesa@appveyor.com>
2022-12-09 21:36:43 -0800

    prototype 3

--- flet-solitaire/proof_of_concept3_a.py
+++ flet-solitaire/proof_of_concept3_a.py
@@ -9,20 +9,6 @@ class GameData:
         self.start_top = 0
         self.start_left = 0
 
-    def place_card(self, card, space):
-        card.top = space.top
-        card.left = space.left
-
-        # remove the card form the old space's pile if exists
-        if card.data.space is not None:
-            card.data.space.data.cards.remove(card)
-
-        # set card's space as new space
-        card.data.space = space
-
-        # add the card to the new space's pile
-        space.data.pile.append(card)
-
     def bounce_back(self, card):
         card.top = self.start_top
         card.left = self.start_left
@@ -37,11 +23,35 @@ class Card:
     def set_control_data(self):
         self.control.data = self
 
+    def place_card(self, space):
+
+        self.control.top = space.top + 20 * len(space.data.pile)
+        self.control.left = space.left
+
+        # remove the card form the old space's pile if exists
+        if self.space is not None:
+            self.space.data.pile.remove(self.control)
+
+        # set card's space as new space
+        self.space = space
+
+        # add the card to the new space's pile
+        space.data.pile.append(self.control)
+
 
 class Space:
     def __init__(self, space):
         self.space = space
         self.pile = []
+        self.set_control_data()
+
+        print(len(self.pile))
+
+    def set_control_data(self):
+        self.space.data = self
+
+    def upper_card_top(self):
+        return self.space.top + 20 * len(self.pile)
 
 
 def main(page: ft.Page):
@@ -61,11 +71,13 @@ def main(page: ft.Page):
     def drop(e: ft.DragEndEvent):
         # check if card is close to any of the spaces
         for space in spaces:
+            new_top = space.data.upper_card_top()
             if (
-                abs(e.control.top - space.top) < 20
+                abs(e.control.top - new_top) < 20
                 and abs(e.control.left - space.left) < 20
             ):
-                game_data.place_card(e.control, space)
+
+                e.control.data.place_card(space)
                 page.update()
                 return
 
@@ -79,31 +91,49 @@ def main(page: ft.Page):
         e.control.update()
 
     spaces = []
+    space_objects = []
 
     # top spaces (foundation piles)
     x = 0
     for i in range(4):
-        spaces.append(
-            ft.Container(width=65, height=100, left=x, top=0, border=ft.border.all(1))
+        space_control = ft.Container(
+            width=65, height=100, left=x, top=0, border=ft.border.all(1)
         )
+        spaces.append(space_control)
+        space_object = Space(space_control)
+        space_objects.append(space_object)
         x += 100
 
     # bottom spaces (plateau piles)
     y = 0
     for i in range(4):
-        spaces.append(
-            ft.Container(width=65, height=100, left=y, top=150, border=ft.border.all(1))
+        space_control = ft.Container(
+            width=65, height=100, left=y, top=150, border=ft.border.all(1)
         )
+        spaces.append(space_control)
+        space_object = Space(space_control)
+        space_objects.append(space_object)
         y += 100
 
-    card1 = ft.GestureDetector(
-        mouse_cursor=ft.MouseCursor.MOVE,
-        drag_interval=10,
-        on_pan_update=move,
-        on_pan_start=start_drag,
-        on_pan_end=drop,
-        content=ft.Container(bgcolor=ft.colors.GREEN, width=65, height=100),
-    )
+    colors = ["BLUE", "YELLOW", "GREEN", "RED"]
+
+    cards = []
+    card_objects = []
+
+    for color in colors:
+
+        cards.append(
+            ft.GestureDetector(
+                mouse_cursor=ft.MouseCursor.MOVE,
+                drag_interval=10,
+                on_pan_update=move,
+                on_pan_start=start_drag,
+                on_pan_end=drop,
+                content=ft.Container(bgcolor=ft.colors.AMBER, width=65, height=100),
+            )
+        )
+        cards[-1].content.bgcolor = color
+        card_objects.append(Card(cards[-1]))
 
     card2 = ft.GestureDetector(
         mouse_cursor=ft.MouseCursor.MOVE,
@@ -114,11 +144,12 @@ def main(page: ft.Page):
         content=ft.Container(bgcolor=ft.colors.AMBER, width=65, height=100),
     )
 
-    cards = [card1, card2]
     game_data = GameData()
 
-    game_data.place_card(card1, spaces[4])
-    game_data.place_card(card2, spaces[5])
+    card_objects[0].place_card(spaces[4])
+    card_objects[1].place_card(spaces[5])
+    card_objects[2].place_card(spaces[6])
+    card_objects[3].place_card(spaces[7])
 
     controls = spaces + cards
 
