aa39c89913b30011763755ff284744cb06a34ca9
InesaFitsner <inesa@appveyor.com>
2022-12-16 12:11:34 -0800

    rules

--- flet-solitaire/solitaire-final/main.py
+++ flet-solitaire/solitaire-final/main.py
@@ -15,7 +15,8 @@ def main(page: ft.Page):
         page.update()
 
     def show_rules(e):
-        print("Solitaire rules")
+        page.dialog = rules_dialog
+        rules_dialog.open = True
         page.update()
 
     def show_settings(e):
@@ -33,6 +34,32 @@ def main(page: ft.Page):
             ft.IconButton(ft.icons.SETTINGS, on_click=show_settings),
             
         ],
+    )
+ 
+    rules_md = ft.Markdown(
+        """
+Klondike is played with a standard 52-card deck, without Jokers.
+
+The four foundations (light rectangles in the upper right of the figure) are built up by suit from Ace (low in this game) 
+to King, and the tableau piles can be built down by alternate colors. Every face-up card in a partial pile, or a complete pile, 
+can be moved, as a unit, to another tableau pile on the basis of its highest card. Any empty piles can be filled with a King, 
+or a pile of cards with a King. The aim of the game is to build up four stacks of cards starting with Ace and ending with King, 
+all of the same suit, on one of the four foundations, at which time the player would have won. There are different ways 
+of dealing the remainder of the deck from the stock to the waste, which can be selected in the Settings:
+
+- Turning three cards at once to the waste, with no limit on passes through the deck.
+- Turning three cards at once to the waste, with three passes through the deck.
+- Turning one card at a time to the waste, with three passes through the deck.
+- Turning one card at a time to the waste with only a single pass through the deck, and playing it if possible.
+- Turning one card at a time to the waste, with no limit on passes through the deck.
+
+If the player can no longer make any meaningful moves, the game is considered lost.
+
+        """)
+
+
+    rules_dialog = ft.AlertDialog(
+        title=ft.Text("Solitaire rules"), content=rules_md, on_dismiss=lambda e: print("Dialog dismissed!")
     )
 
     solitaire = Solitaire()
