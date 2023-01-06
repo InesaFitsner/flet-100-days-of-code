97a3d518f79b890c2a2022e795c115df6a78931d
InesaFitsner <inesa@appveyor.com>
2023-01-06 13:56:33 -0800

    moved slot and card creation to Solitaire

--- flet-solitaire/solitaire-fanned-piles/solitaire.py
+++ flet-solitaire/solitaire-fanned-piles/solitaire.py
@@ -3,6 +3,8 @@ SOLITAIRE_WIDTH = 1000
 SOLITAIRE_HEIGHT = 500
 
 import flet as ft
+from slot import Slot
+from card import Card
 
 class Solitaire(ft.Stack):
     def __init__(self):
@@ -14,6 +16,25 @@ class Solitaire(ft.Stack):
         self.card_offset = CARD_OFFSET
         self.width = SOLITAIRE_WIDTH
         self.height = SOLITAIRE_HEIGHT
+
+    def did_mount(self):
+        self.create_slots()
+        self.create_card_deck()
+
+    def create_slots(self):
+        slot1 = Slot(top=0, left=200)
+        slot2 = Slot(top=0, left=300)
+        slots = [slot1, slot2]
+        self.slots.extend(slots)
+        self.controls.extend(self.slots)
+        self.update()
+
+    def create_card_deck(self):
+        card1 = Card(self, color="GREEN", top=0, left=0)
+        card2 = Card(self, color="YELLOW", top=0, left=100)
+        cards = [card1, card2]
+        self.controls.extend(cards)
+        self.update()
 
     def move_on_top(self, draggable_pile):
         """Brings draggable card pile to the top of the stack"""
