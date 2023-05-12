4fac822c17c82470444b841a5c6ee4670b08877e
InesaFitsner <inesa@appveyor.com>
2023-05-12 11:49:54 -0700

    Update color_picker.py

--- test_projects/color_picker.py
+++ test_projects/color_picker.py
@@ -2,13 +2,14 @@ import flet as ft
 import colorsys
 
 WIDTH = 50
-HEIGHT = 50
+HEIGHT = 25
+SQUARE_SIZE = 5
+CIRCLE_SIZE = SQUARE_SIZE*5
 
 class CustomColorPicker(ft.AlertDialog):
     def __init__(self):
         super().__init__()
         self.content = ft.Column()
-
         self.generate_color_matrix(hue=0)
         self.on_dismiss=lambda e: print("Dialog dismissed!")
     
@@ -17,45 +18,40 @@ class CustomColorPicker(ft.AlertDialog):
             return '#{:02x}{:02x}{:02x}'.format(int(rgb[0]*255.0), int(rgb[1]*255.0), int(rgb[2]*255.0))
         
         selected_color = ft.Text()
-        #colors = ft.Column(spacing=0)
-        colors = ft.Stack(height=50*5, width=50*5)
+        colors = ft.Stack(
+            height=HEIGHT*SQUARE_SIZE+CIRCLE_SIZE, 
+            width=WIDTH*SQUARE_SIZE+CIRCLE_SIZE
+            )
         self.content.controls = []
         #colors.controls = []
 
         def pick_color(e):
-            circle.top = e.control.top
-            circle.left = e.control.left
+            circle.top = e.control.top - CIRCLE_SIZE/2
+            circle.left = e.control.left - CIRCLE_SIZE/2
             circle.bgcolor = e.control.bgcolor
             circle.update()
             selected_color.value = e.control.bgcolor
             selected_color.update()
 
         for j in range (0, HEIGHT):
-            #colors.controls.append(ft.Row(spacing=0))
             for i in range (0, WIDTH):
                 #color = rgb2hex(colorsys.hsv_to_rgb(i/WIDTH,  1, 1 * (HEIGHT - j + 1) / HEIGHT))
                 color = rgb2hex(colorsys.hsv_to_rgb(hue,  (i) /WIDTH, 1 * (HEIGHT - j) / HEIGHT))
-                # colors.controls[-1].controls.append(ft.Container(
-                #     height=5, 
-                #     width=5, 
-                #     bgcolor=color,
-                #     on_click=pick_color
-                #     ))
                 colors.controls.append(ft.Container(
-                    height=5, 
-                    width=5, 
+                    height=SQUARE_SIZE, 
+                    width=SQUARE_SIZE, 
                     bgcolor=color, 
                     on_click=pick_color,
-                    top = j*5,
-                    left = i*5  ))
+                    top = j*SQUARE_SIZE+CIRCLE_SIZE/2,
+                    left = i*SQUARE_SIZE+CIRCLE_SIZE/2  ))
 
         circle = ft.Container(
             top = 0,
             left = 0,
-            width=30,
-            height=30,
+            width=CIRCLE_SIZE,
+            height=CIRCLE_SIZE,
             bgcolor='blue',
-            border_radius=30,
+            border_radius=SQUARE_SIZE*5,
             border=ft.border.all(width=2, color='white'))
 
         colors.controls.append(circle)
